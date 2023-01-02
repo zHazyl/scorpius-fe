39cbe355faf552776aab557ed1c763f3b693aa9f
zHazyl <tnh210302@gmail.com>
2023-01-02 11:10:27 +0700

    ok

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,16 @@
 
 export const environment = {
   production: false,
-  baseApiUrl: 'http://34.70.216.186:8080',
-  websocketBaseUrl: 'ws://34.70.216.186:8080',
+  baseApiUrl: 'http://34.123.206.104:8080',
+  websocketBaseUrl: 'ws://34.123.206.104:8080',
   authServiceResource: '/auth-service',
   chatServiceResource: '/chat-service',
   chatMessagesServiceResource: '/chat-messages-service',
   websocketMessageServiceResource: '/messages-websocket-service'
 };
 
-export const baseUrl = 'http://34.70.216.186:8080';
-export const wsBaseUrl = 'ws://34.70.216.186:8080';
+export const baseUrl = 'http://34.123.206.104:8080';
+export const wsBaseUrl = 'ws://34.123.206.104:8080';
 
 /*
  * For easier debugging in development mode, you can import the following file
